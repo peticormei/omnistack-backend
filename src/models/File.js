0b602726823f4cff89398100e0dbbdec9e054479
Filipe Mei <peticormei@gmail.com>
2019-04-20 22:38:00 -0300

    first commit

---
+++ src/models/File.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const File = new mongoose.Schema({
+    title: { type: String, required: true },
+    path: { type: String, required: true }
+}, { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } });
+
+File.virtual('url').get(function() {
+    return `http://localhost:3000/files/${encodeURIComponent(this.path)}`;
+});
+
+module.exports = mongoose.model('File', File);
---
+++ src/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
+const mongoose = require('mongoose');
+const path = require('path');
+
+const app = express();
+app.use(cors());
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+    socket.on('connectionRoom', box => {
+        socket.join(box);
+    });
+
+    console.log('ok');
+});
+
+mongoose.connect(
+    'mongodb+srv://omnistack:omnistack@cluster0-klxyc.mongodb.net/test?retryWrites=true', 
+    {
+        useNewUrlParser: true
+    }
+);
+
+
+// MiddleWares
+app.use((req, res, next) => {
+    req.io = io
+    return next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
+
+app.use(routes);
+
+server.listen(3000, () => {
+    console.log('server running on port 3000');
+});
